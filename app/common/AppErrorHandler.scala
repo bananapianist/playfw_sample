2f77fbe0fa1fbdf0009c49fdf69dc2a957bcf908
Dr.Vinatmina <akimitsu.hamaguchi@dragon.jp>
2016-03-28 17:25:26 +0900

    カスタム404実装

---
+++ app/common/AppErrorHandler.scala
@@ -0,0 +1,24 @@
+package common
+
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import play.Logger
+
+class AppErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    Future.successful(
+        //Ok(errors.views.html.404())
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    Logger.debug("server error")
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
