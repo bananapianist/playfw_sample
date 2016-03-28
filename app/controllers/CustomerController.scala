d0d4c2e6f91a523e84ebe5bbc3f8624afadbb361
Dr.Vinatmina <akimitsu.hamaguchi@dragon.jp>
2016-03-28 19:16:48 +0900

    テンプレートのサブディレクトリ化

--- app/controllers/CustomerController.scala
+++ app/controllers/CustomerController.scala
@@ -34,12 +34,12 @@ class CustomerController @Inject()(addToken: CSRFAddToken, checkToken: CSRFCheck
   
   def index = addToken{
     Action.async { 
-      customerDao.all().map(customers => Ok(views.html.customerlist("", customers)))
+      customerDao.all().map(customers => Ok(views.html.customer.list("", customers)))
     }
   }
   def add = addToken{
     Action {implicit request =>
-      Ok(views.html.customerregist("", CustomerForm.form))
+      Ok(views.html.customer.regist("", CustomerForm.form))
     }
   }
   
@@ -48,13 +48,13 @@ class CustomerController @Inject()(addToken: CSRFAddToken, checkToken: CSRFCheck
       CustomerForm.form.bindFromRequest.fold(
           formWithErrors => {
             Logger.debug(formWithErrors.toString())
-            Future(BadRequest(views.html.customerregist("エラー", formWithErrors)))
+            Future(BadRequest(views.html.customer.regist("エラー", formWithErrors)))
           },
           customer => {
             customerDao.create(customer).flatMap(cnt =>
-                //if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customerlist("登録しました", customers)))
+                //if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customer.list("登録しました", customers)))
                 if (cnt != 0) Future.successful(Redirect(routes.CustomerController.index))
-                else customerDao.all().map(notifications => BadRequest(views.html.customeredit("エラー", CustomerForm.form.fill(customer))))
+                else customerDao.all().map(notifications => BadRequest(views.html.customer.edit("エラー", CustomerForm.form.fill(customer))))
              )
           }
       )
@@ -65,8 +65,8 @@ class CustomerController @Inject()(addToken: CSRFAddToken, checkToken: CSRFCheck
     Action.async {implicit request =>
       customerDao.findById(customerId).flatMap(option =>
         option match {
-          case Some(customer) => Future(Ok(views.html.customeredit("GET", CustomerForm.form.fill(customer))))
-          case None => customerDao.all().map(customers => BadRequest(views.html.customerlist("エラー", customers)))
+          case Some(customer) => Future(Ok(views.html.customer.edit("GET", CustomerForm.form.fill(customer))))
+          case None => customerDao.all().map(customers => BadRequest(views.html.customer.list("エラー", customers)))
         }
       )
     }
@@ -76,13 +76,13 @@ class CustomerController @Inject()(addToken: CSRFAddToken, checkToken: CSRFCheck
     Action.async { implicit request =>
       CustomerForm.form.bindFromRequest.fold(
           formWithErrors => {
-            Future(BadRequest(views.html.customeredit("ERROR", formWithErrors)))
+            Future(BadRequest(views.html.customer.edit("ERROR", formWithErrors)))
           },
           customer => {
             customerDao.update_mappinged(customer).flatMap(cnt =>
-              if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customerlist("更新しました", customers)))
+              if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customer.list("更新しました", customers)))
               //if (cnt != 0) Future.successful(Redirect(routes.CustomerController.index))
-              else customerDao.all().map(notifications => BadRequest(views.html.customeredit("エラー", CustomerForm.form.fill(customer))))
+              else customerDao.all().map(notifications => BadRequest(views.html.customer.edit("エラー", CustomerForm.form.fill(customer))))
             )
           }
       )
@@ -92,8 +92,8 @@ class CustomerController @Inject()(addToken: CSRFAddToken, checkToken: CSRFCheck
     def delete(id: Long) = checkToken{
       Action.async {
         customerDao.delete(id).flatMap(cnt =>
-          if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customerlist("削除しました", customers)))
-          else customerDao.all().map(customers => BadRequest(views.html.customerlist("エラー", customers)))
+          if (cnt != 0) customerDao.all().map(customers => Ok(views.html.customer.list("削除しました", customers)))
+          else customerDao.all().map(customers => BadRequest(views.html.customer.list("エラー", customers)))
         )
       }
     }
