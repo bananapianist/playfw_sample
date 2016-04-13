f39ed67ef43274ad085aa48cc4a809349c81bde6
Dr.Vinatmina <akimitsu.hamaguchi@dragon.jp>
2016-04-13 19:11:04 +0900

    selectタグのオプション作成ヘルパー追加

--- app/utilities/ViewHelper.scala
+++ app/utilities/ViewHelper.scala
@@ -3,6 +3,11 @@ package utilities
 import play.twirl.api.HtmlFormat
 import java.util.Date
 
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
+
+ import play.api.Logger
+
 object ViewHelper {
   def nl2br(inputstr: Option[String], withescape:Boolean = true):String ={
     inputstr match{
@@ -21,5 +26,28 @@ object ViewHelper {
       case Some(date) => formatstr format date
       case _ => ""
     }
+  }
+  
+  def makeOptionSeq(optionvalue:Seq[String], messagekeyname:String, messages:play.api.i18n.Messages) ={
+    this.makeSeqWithMessageApi(optionvalue, messagekeyname, messages).toSeq.sortBy(_._1)
+  }
+  def makeOptionMap(optionvalue:Seq[String], messagekeyname:String, messages:play.api.i18n.Messages) ={
+    this.makeSeqWithMessageApi(optionvalue, messagekeyname, messages)
+  }
+  def makeSeqWithMessageApi( s:Seq[String] , messagekeyname:String, messages:play.api.i18n.Messages) = {
+    def count( l:Seq[String], m:Map[String, String] ):Map[String, String] = {
+      if( l.isEmpty ){  // lが空Listだったら再帰終了
+        m
+      }else {
+        val str = messages(messagekeyname + '.' + l.head )
+        count( l.tail, m ++ Map( l.head -> str ) )
+      }
+    }
+    
+     count( s, Map.empty[String,String] )
+  }
+  def getOptionView(optionval:String, maplist:Map[String, String]):String = {
+    maplist(optionval)
+    
   }
 }
