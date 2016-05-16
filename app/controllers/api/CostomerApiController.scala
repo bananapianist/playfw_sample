7f52b298030b3f12869457b1a97a3c96376d7a0c
Dr.Vinatmina <akimitsu.hamaguchi@dragon.jp>
2016-05-16 22:29:00 +0900

    Jason⇔case class変換実装

--- app/controllers/api/CostomerApiController.scala
+++ app/controllers/api/CostomerApiController.scala
@@ -17,15 +17,28 @@ import scalaoauth2.provider._
 import scalaoauth2.provider.OAuth2ProviderActionBuilders._
 
 import play.api.libs.json._
-import skinny.util.JSONStringOps._
 
 class CostomerApiController @Inject()(oauth2DataHandler: Oauth2DataHandler, customerDao: CustomerDAO) extends Controller with OAuth2Provider {
-  
+  implicit val residentWrites = new Writes[CustomerRow] {
+  def writes(customer: CustomerRow) = Json.obj(
+    "id" -> customer.id,
+    "name" -> customer.name,
+    "email" -> customer.email,
+    "tel" -> customer.tel,
+    "address" -> customer.address,
+    "comment" -> customer.comment,
+    "actionDate" -> customer.actionDate,
+    "notificationPeriod" -> customer.notificationPeriod,
+    "notificationDate" -> customer.notificationDate,
+    "isDisabled" -> customer.isDisabled,
+    "createdDate" -> customer.createdDate,
+    "updatedDate" -> customer.updatedDate  )
+  }
   def getlist =  Action.async { implicit request =>
     authorize(oauth2DataHandler) { authInfo =>
       val user = authInfo.user 
-      customerDao.all().flatMap { customer => 
-        Future.successful(Ok(Json.toJson(customer.toList.toString()))) 
+      customerDao.all().flatMap { customers => 
+        Future.successful(Ok(Json.toJson(customers))) 
       }
      
     }
